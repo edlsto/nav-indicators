8cb5010b388d67b631aa5d622bcd91c47d9f8f42
Edward Stoner <edwardlstoner@gmail.com>
2019-12-20 10:20:14 -0700

    Add makeActive function

--- main.js
+++ main.js
@@ -3,3 +3,24 @@ var navItems = document.querySelectorAll('.nav-item');
 var mainContent = document.querySelector('main');
 
 navbar.addEventListener('click', makeActive);
+
+
+function makeActive(event) {
+  if (event.target.classList.contains('collections')) {
+      removeHighlight();
+      event.target.classList.add('active');
+      addCollectionsHTML();
+  } else if (event.target.classList.contains('wishlist')) {
+      removeHighlight();
+      event.target.classList.add('active');
+      addWishlistHTML();
+  } else if (event.target.classList.contains('archived')) {
+      removeHighlight();
+      event.target.classList.add('active');
+      addArchivedHTML();
+  } else if (event.target.classList.contains('all-courses')) {
+      removeHighlight();
+      event.target.classList.add('active');
+      addAllCoursesHTML();
+  }
+}
