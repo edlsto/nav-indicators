a38a7740114915b92636918f0bcf7a0161898884
Edward Stoner <edwardlstoner@gmail.com>
2019-12-20 10:19:53 -0700

    Add event listener

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 var navbar = document.querySelector('ul');
 var navItems = document.querySelectorAll('.nav-item');
 var mainContent = document.querySelector('main');
+
+navbar.addEventListener('click', makeActive);
